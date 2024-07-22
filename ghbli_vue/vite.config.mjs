ceabefa5304d2aaee0a0338a50fe891e72266b1f
yoonji <kyjkbw@gmail.com>
2024-07-22 22:10:43 +0900

    vite.config  base값 수정

--- ghbli_vue/vite.config.mjs
+++ ghbli_vue/vite.config.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "GhibliFilms",
+  base: '/GhibliFilms/',
   plugins: [
     VueRouter(),
     Layouts(),
